02d62caad5abc4cfea5fbc35ff485895f1636879
Cindy Tong <tongcindyy@gmail.com>
2020-01-19 21:38:28 -0500

    Test frontend auth ajax mount to window

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
-
         if @user.save
             login(@user)
             render "api/users/show"
@@ -13,7 +12,6 @@ class Api::UsersController < ApplicationController
     private 
 
     def user_params 
-        debugger
         params.require(:user).permit(:email, :first_name, :last_name, :password, :over_age_13)
     end 
 end 
